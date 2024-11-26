283ff8a41c6ce3882f8721fe80ed5a8fcfdcd13c
Edil Pizzolato Junior <gunior.pizzolato@gmail.com>
2024-11-26 09:54:39 -0300

    -Finalizado Projeto do curso da Alura "Go: desenvolvendo uma API Rest"

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-rest-api/database"
+	"go-rest-api/routes"
+)
+
+func main() {
+	database.ConectaComBancoDeDados()
+
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.Handlerequest()
+}
